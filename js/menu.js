02f82134c6035382ccf66b6c8e7657b8cd490c17
AmFennyFox <11378516+AmFennyFox@users.noreply.github.com>
2019-11-08 13:20:51 +0100

    Yea it broke
    Someone tell me how to fix this shit please

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
 	jQuery('.toggle-nav').click(function(e) {
 		jQuery(this).toggleClass('active');
-		jQuery('.menu ul').toggleClass('active');
+		jQuery('.navmenu ul').toggleClass('active');
 
 		e.preventDefault();
 	});
